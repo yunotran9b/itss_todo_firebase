18ad0200701c6048ebe077ce690f25833cd0079b
Tran Nhat Thong <yunotran123@gmail.com>
2021-05-21 20:56:05 +0700

    Save Todo on Firestore

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyBGcuh_AgWraIi48BJ6i_8JnL6IuWtKLOg",
+    authDomain: "fir-sample-52db0.firebaseapp.com",
+    projectId: "fir-sample-52db0",
+    storageBucket: "fir-sample-52db0.appspot.com",
+    messagingSenderId: "436477464399",
+    appId: "1:436477464399:web:16146f48501e681ac9ff3a"
+}
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig)
